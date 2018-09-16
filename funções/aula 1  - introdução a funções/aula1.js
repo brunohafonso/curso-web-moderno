4dacb7bd2dded31f49cc5b777b8955340d902b20
Bruno Afonso <brunohafonso@gmail.com>
2018-09-16 13:13:59 -0300

    capitulo de funções

---
+++ funções/aula 1  - introdução a funções/aula1.js
@@ -0,0 +1,33 @@
+// Criando uma função de forma literal
+function func1() {
+
+}
+
+// Armazenando uma função numa variavel
+const func2 = function() {
+
+}
+
+// Armazendo a função em um array
+const array = [function(a, b) { return a + b }, func1, func2];
+console.log(array[0](2, 3)); // imprime 5
+
+// Armazenando no atributo de um objeto
+const obj = {};
+obj.falar = () => 'bla bla bla';
+console.log(obj.falar()); // imprime bla bla bla
+
+// Passar uma função como parametro de outra
+function run(func) {
+    func();
+}
+
+run(function() { console.log('função executada.'); }); // imprime função executada.
+
+// Um função retornar/conter uma função
+function soma(a, b) {
+    return function(c) {
+        console.log(a + b + c);
+    }
+}
+soma(2, 3)(4); // imprime 9
